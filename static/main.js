a3c35c2b2dbd502687da1e813a7cc3e9c2f6eec4
Samuel Braun <slb2250@columbia.edu>
2024-03-09 05:54:37 -0500

    contrast colors workinggit add static templates

--- static/main.js
+++ static/main.js
@@ -1,71 +1,46 @@
 function setPredominantBackgroundColor(imageElement, containerElement) {
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-    const img = new Image();
+    return new Promise((resolve, reject) => {
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        const img = new Image();
 
-    img.onload = function () {
-        // Set canvas size to the image size
-        canvas.width = this.width;
-        canvas.height = this.height;
-        context.drawImage(this, 0, 0, this.width, this.height);
+        img.onload = function () {
+            canvas.width = this.width;
+            canvas.height = this.height;
+            context.drawImage(this, 0, 0, this.width, this.height);
 
-        // Get image data
-        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-        const data = imageData.data;
-        const colorMap = {};
+            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+            const data = imageData.data;
+            const colorMap = {};
 
-        // Generate a color map with frequency of each color
-        for (let i = 0; i < data.length; i += 4) {
-            // Convert color to a string in the form of 'r,g,b'
-            const color = `${data[i]},${data[i + 1]},${data[i + 2]}`;
-
-            if (colorMap[color]) {
-                colorMap[color]++;
-            } else {
-                colorMap[color] = 1;
+            for (let i = 0; i < data.length; i += 4) {
+                const color = `${data[i]},${data[i + 1]},${data[i + 2]}`;
+                colorMap[color] = (colorMap[color] || 0) + 1;
             }
-        }
 
-        // Determine the most frequent color
-        let predominantColor = { color: null, count: 0 };
-        for (const [color, count] of Object.entries(colorMap)) {
-            if (count > predominantColor.count) {
-                predominantColor = { color, count };
+            let predominantColor = { color: null, count: 0 };
+            for (const [color, count] of Object.entries(colorMap)) {
+                if (count > predominantColor.count) {
+                    predominantColor = { color, count };
+                }
             }
-        }
 
-        // Set container background to predominant color
-        containerElement.style.backgroundColor = `rgb(${predominantColor.color})`;
-        new_color = predominantColor.color;
-        var r = new_color.r;
-        var g = new_color.g;
-        var b = new_color.b;
-        var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
-        // containerElement.style.color = rgb(yiq, yiq, yiq);
-    };
+            containerElement.style.backgroundColor = `rgb(${predominantColor.color})`;
 
-    // Handle CORS security if the images are not served from the same domain
-    img.crossOrigin = 'Anonymous';
+            const rgbValues = predominantColor.color.split(',').map(Number);
+            const contrastColor = getContrastYIQ({ r: rgbValues[0], g: rgbValues[1], b: rgbValues[2] });
 
-    // Start loading the image
-    img.src = imageElement.src;
+            resolve({ predominantColor: `rgb(${predominantColor.color})`, contrastColor });
+        };
 
-    // const rgbValues = predominantColor.color.split(',').map(Number);
-    // const contrastColor = getContrastYIQ({ r: rgbValues[0], g: rgbValues[1], b: rgbValues[2] });
-    // containerElement.style.color = contrastColor;
+        img.onerror = reject;
 
-    const [r, g, b] = predominantColor.split(',').map(c => parseInt(c));
-    const contrastColor = getContrastYIQ({r, g, b});
-
-    return contrastColor.color;
-
-
-
-
-
+        img.crossOrigin = 'Anonymous';
+        img.src = imageElement.src;
+    });
 }
 
-function getContrastYIQ(rgb) {
-    const yiq = ((rgb.r * 299) + (rgb.g * 587) + (rgb.b * 114)) / 1000;
+function getContrastYIQ({ r, g, b }) {
+    const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
     return (yiq >= 128) ? 'black' : 'white';
 }
